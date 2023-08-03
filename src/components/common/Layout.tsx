ad3930b7a1adb8c964f1d3863ec5d9f31fb56f6a
shinheeje <dbfk2637@naver.com>
2023-08-03 18:29:09 +0900

    Design : 레이아웃 설정

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { styled } from 'styled-components'
 
 const Layout = () => {
-  return <Outlet />
+  return (
+    <BackgroundColor>
+      <MaxWidth>
+        <Outlet />
+      </MaxWidth>
+    </BackgroundColor>
+  )
 }
 
+const BackgroundColor = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.gray.TO};
+`
+
+const MaxWidth = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+`
 export default Layout
