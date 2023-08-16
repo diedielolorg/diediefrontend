bf09dfa0db810b13b994061e19bdbafaa2c5a5d0
hansololiviakim <dev.hansolkim@gmail.com>
2023-08-16 18:42:12 +0900

    Feat : API res값 가데이터 추가, ReportList 컴포넌트 분리

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -4,5 +4,6 @@ import Button from './Button'
 import Badge from './Badge'
 import Image from './Image'
 import Tier from './Tier'
+import ReportList from './ReportList'
 
-export { Layout, Portal, Button, Badge, Image, Tier }
+export { Layout, Portal, Button, Badge, Image, Tier, ReportList }
