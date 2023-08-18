3e2bfe26c11cc75f21c16e8cb35721d700b184de
hansololiviakim <dev.hansolkim@gmail.com>
2023-08-19 00:59:33 +0900

    Design : ZoomImg width, height 최소/최대값 설정

--- src/interfaces/ModalTypes.ts
+++ src/interfaces/ModalTypes.ts
@@ -3,6 +3,12 @@ export interface UserListDivProps {
   $position: string
 }
 
+// ZoomImg.tsx
+export interface ZoomImgProps {
+  $width?: number | null
+  $height?: number | null
+}
+
 // SnackBar.ts
 export interface snackBar {
   type?: string
