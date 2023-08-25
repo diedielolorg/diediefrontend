3e8e456410d59a845a9228b1ad39d847ea423f5a
godjooyoung <xolovesjoo36@naver.com>
2023-08-26 03:01:56 +0900

    Design : main ui 작업, 랜덤팁 추가. 검색어 하이라이팅 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,36 +2,39 @@ import React, { useState } from 'react'
 import { styled } from 'styled-components'
 import { Image } from '../components/common'
 import { logo, searchBtn } from '../assets'
+import tips from '../utils/tipsData'
 
 const Main = () => {
   // 검색어
   const [searchKeyword, setSearchKeyword] = useState('')
 
+  const randomTip = tips[Math.floor(Math.random() * tips.length)]
+
   // ! 임시 08.21. api 명세에 따른 임시 검색 목록 리스트
   const [searchSummonerList, setSearchSummonerList] = useState<{ summonerName: string; summonerIcon: string }[]>([
     {
-      summonerName: '방배동 오소리',
+      summonerName: '방랑오소리',
       summonerIcon:
         'https://github.com/diedielolorg/diediefrontend/assets/58963027/f6b4ca75-dec5-4aea-8d52-7ef9d4179915',
     },
     {
-      summonerName: '방배 휴그랜트',
+      summonerName: '방배동휴그랜트',
       summonerIcon:
         'https://github.com/diedielolorg/diediefrontend/assets/58963027/f6b4ca75-dec5-4aea-8d52-7ef9d4179915',
     },
   ])
 
-  // ! 임시 08.21. api 명세에 따른 임시 검색 목록 리스트
+  // ! 임시 08.21.
   const [searchSummonerList1, setSearchSummonerList1] = useState<{ summonerName: string; summonerIcon: string }[]>([])
 
-  // 검색 입력 이벤트 핸들러  -> ! 임시 08.25. 커스텀 훅으로 뺄 것
+  // 검색 입력 이벤트 핸들러
   const searchKeywordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchKeyword(e.target.value)
   }
 
   return (
-    <>
-      <Image src={logo} alt={'로고'} />
+    <WrapMainBoxDiv>
+      <Image width={338} height={66} src={logo} alt={'로고'} />
       <SearchBoxDiv>
         <SearchInputBoxDiv>
           <SearchInput type={'text'} placeholder={'소환사명을 검색하세요.'} onChange={searchKeywordHandler} />
@@ -43,31 +46,51 @@ const Main = () => {
           {searchSummonerList.length > 0 ? (
             searchKeyword &&
             searchSummonerList &&
-            searchSummonerList.map((searchSummoner, idx) => {
+            searchSummonerList.map(({ summonerName, summonerIcon }, idx) => {
+              const regex = new RegExp(`(${searchKeyword})`, 'gi') // 검색어 일치 정규식
+              const parts = summonerName.split(regex)
               return (
                 <SearchSummonerBoxDiv>
                   <SearchSummonerInfoDiv>
-                    <Image width={26} height={26} src={searchSummoner.summonerIcon} />
-                    <SearchSummonerNameP>{searchSummoner.summonerName}</SearchSummonerNameP>
+                    <Image width={26} height={26} src={summonerIcon} />
+                    <SearchSummonerNameP isResult>
+                      {parts.map((part, idx) =>
+                        part.toLowerCase() === searchKeyword.toLowerCase() ? (
+                          <HighLightedTextSpan>{part}</HighLightedTextSpan>
+                        ) : (
+                          <span>{part}</span>
+                        ),
+                      )}
+                    </SearchSummonerNameP>
                   </SearchSummonerInfoDiv>
                 </SearchSummonerBoxDiv>
               )
             })
           ) : (
-            <>
-              <div>{'dd'}</div>
-              <div>{'없음'}</div>
-            </>
+            <SearchSummonerBoxDiv>
+              <SearchSummonerInfoDiv>
+                <SearchSummonerNameP isResult={false}>{'검색된 소환사가 없습니다.'}</SearchSummonerNameP>
+              </SearchSummonerInfoDiv>
+            </SearchSummonerBoxDiv>
           )}
         </SearchResultBoxDiv>
+        <TipBoxDiv>{randomTip}</TipBoxDiv>
       </SearchBoxDiv>
-      <div>{'팁들자리'}</div>
-    </>
+    </WrapMainBoxDiv>
   )
 }
 
 export default Main
 
+const WrapMainBoxDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 54px;
+  margin-top: 143px;
+  /* TODO 반응형 추가해야함 */
+`
 const SearchInputBoxDiv = styled.div`
   display: flex;
   width: 856px;
@@ -76,9 +99,11 @@ const SearchInputBoxDiv = styled.div`
   background: ${({ theme }) => theme.color.white};
 `
 const SearchBoxDiv = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   gap: 10px;
+  align-items: center;
 `
 const SearchInput = styled.input`
   flex: 1;
@@ -101,6 +126,7 @@ const SearchBtn = styled.button`
   background: ${({ theme }) => theme.green.basic};
 `
 const SearchResultBoxDiv = styled.div<{ searchKeyword: string }>`
+  z-index: 1;
   width: 856px;
   border-radius: 10px;
   background: ${({ theme }) => theme.color.white};
@@ -118,10 +144,24 @@ const SearchSummonerInfoDiv = styled.div`
   margin-left: 36px;
   margin-top: 26px;
 `
-const SearchSummonerNameP = styled.p`
-  color: #060606;
+const SearchSummonerNameP = styled.p<{ isResult: boolean }>`
+  color: ${(props) => (props.isResult ? '#060606' : '#727272')};
   font-size: 20px;
   font-style: normal;
   font-weight: 500;
   line-height: normal;
+`
+const HighLightedTextSpan = styled.span`
+  color: ${({ theme }) => theme.green.dark};
+`
+const TipBoxDiv = styled.div`
+  position: absolute;
+  top: calc(86px + 27px);
+  z-index: 0;
+  color: ${({ theme }) => theme.color.white};
+  text-align: center;
+  text-shadow: 0px 0px 10px ${({ theme }) => theme.color.black};
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
 `
