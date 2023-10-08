dd2eccd95dc46e6930c93ade5ac24fe9228dfd8d
hansololiviakim <dev.hansolkim@gmail.com>
2023-10-09 04:46:58 +0900

    Refactor : 다국어 로직 수정, 메인페이지 turquoise 컬러 적용

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { styled } from 'styled-components'
 import { useTranslation } from 'react-i18next'
-import headerLogo from '../../assets/headerLogo.svg'
-import { langKo, langEn } from '../../assets'
-import { Languages } from '../../utils/Locales/i18n'
+import { neonHeaderLogo, headerLogo, neonLangKo, langKo, neonLangEn, langEn } from '../../assets'
 
 const Header = () => {
   const { t, i18n } = useTranslation()
+  const currentLang = i18n.language
   const navigate = useNavigate()
+  const path = useLocation().pathname
 
-  const [isLogin, setIsLogin] = useState<boolean>(false)
-  const [isEnglish, setIsEnglish] = useState<boolean>(true)
+  const [isLogin, setIsLogin] = useState(false)
+  const [isKorean, setIsKorean] = useState(currentLang.includes('ko'))
 
   useEffect(() => {
     const checkLoginStatusHandler = () => {
@@ -31,33 +31,43 @@ const Header = () => {
     navigate('/')
   }
 
-  const translateBtnHandler = (lang: Languages) => {
-    setIsEnglish((prevState) => !prevState)
-    i18n.changeLanguage(lang)
+  const translateBtnHandler = () => {
+    const setLang = currentLang.includes('ko') ? 'en' : 'ko'
+    setIsKorean(setLang === 'ko')
+    i18n.changeLanguage(setLang)
   }
 
   return (
     <WrapHeader>
-      <Logo type={'button'} onClick={() => navigate('/')} />
+      <IconDiv onClick={() => navigate('/')}>
+        <img width={'53px'} height={'40px'} src={path === '/' ? neonHeaderLogo : headerLogo} alt={'헤더 로고'} />
+      </IconDiv>
       <Menu>
-        <MenuBtn type={'button'} onClick={() => navigate('/ranking')}>
+        <MenuBtn type={'button'} onClick={() => navigate('/ranking')} $path={path}>
           {t('랭킹')}
         </MenuBtn>
         {isLogin ? (
-          <MenuBtn type={'button'} onClick={() => navigate('/mypage')}>
-            {'마이페이지'}
-          </MenuBtn>
-        ) : null}
-        {isLogin ? (
-          <MenuBtn type={'button'} onClick={moveToSignOutBtnHandler}>
-            {'로그아웃'}
-          </MenuBtn>
+          <>
+            <MenuBtn type={'button'} onClick={() => navigate('/mypage')} $path={path}>
+              {t('마이페이지')}
+            </MenuBtn>
+            <MenuBtn type={'button'} onClick={moveToSignOutBtnHandler} $path={path}>
+              {t('로그아웃')}
+            </MenuBtn>
+          </>
         ) : (
-          <MenuBtn type={'button'} onClick={() => navigate('/signin')}>
+          <MenuBtn type={'button'} onClick={() => navigate('/signin')} $path={path}>
             {t('로그인')}
           </MenuBtn>
         )}
-        <TransBtn type={'button'} onClick={() => translateBtnHandler(isEnglish ? 'en' : 'ko')} isEnglish={isEnglish} />
+        <IconDiv onClick={translateBtnHandler}>
+          <img
+            width={'55px'}
+            height={'32px'}
+            src={path === '/' ? (isKorean ? neonLangKo : neonLangEn) : isKorean ? langKo : langEn}
+            alt={'다국어 변환 아이콘'}
+          />
+        </IconDiv>
       </Menu>
     </WrapHeader>
   )
@@ -70,41 +80,27 @@ const WrapHeader = styled.header`
   height: 88px;
   background-color: ${({ theme }) => theme.color.black};
   display: flex;
-  @media screen and (max-width: 1440px) {
-    gap: 508px;
-  }
   justify-content: space-around;
   align-items: center;
 `
 
-const Logo = styled.button`
-  background-color: transparent;
-  cursor: pointer;
-  background-image: url(${headerLogo});
-  width: 53px;
-  height: 40px;
+const IconDiv = styled.div`
+  img {
+    cursor: pointer;
+  }
 `
 
 const Menu = styled.div`
   display: flex;
-  min-width: 500px;
-  justify-content: flex-end;
-  gap: 85px;
+  gap: 60px;
 `
 
-const MenuBtn = styled.button`
-  color: ${({ theme }) => theme.green.basic};
+const MenuBtn = styled.button<{ $path: string }>`
+  width: 90px;
+  color: ${({ $path, theme }) => ($path === '/' ? theme.green.turquoise : theme.green.basic)};
   background-color: transparent;
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   font-size: 20px;
   font-style: normal;
   font-weight: 600;
-  cursor: pointer;
-`
-const TransBtn = styled.button<{ isEnglish: boolean }>`
-  cursor: pointer;
-  background-image: url(${(props) => (props.isEnglish ? langKo : langEn)});
-  width: 55px;
-  height: 32px;
-  background-color: transparent;
 `
